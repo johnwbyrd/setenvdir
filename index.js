e3431f919ef39142281531d22e94de49f2a17413
John Byrd <jbyrd@giganticsoftware.com>
2020-01-10 21:59:38 -0800

    Fix string replace method in Javascript

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const github = require('@actions/github');
 
 try {
   const inputPath = core.getInput('path');
-  outputPath = inputPath;
-  outputPath = str.replace(/\\/g, inputPath);
+  outputPath = inputPath.replace(/\\/g, '/');
   core.setOutput('path', outputPath);
   core.exportVariable('CMAKE_INSTALL_DIR', outputPath.concat('/install'));
 } catch (error) {
