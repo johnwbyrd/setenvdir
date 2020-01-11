7a7e3a79c902d62202ce7bb54f724d4e19330a7d
John Byrd <jbyrd@giganticsoftware.com>
2020-01-10 22:07:42 -0800

    Add debugging log line

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ try {
   const inputPath = core.getInput('path');
   outputPath = inputPath.replace(/\\/g, '/');
   core.setOutput('path', outputPath);
+  installdir = outputPath.concat('/install');
+  console.log(`CMAKE_INSTALL_DIR is about to be: ${installdir}`);
   core.exportVariable('CMAKE_INSTALL_DIR', outputPath.concat('/install'));
 } catch (error) {
   core.setFailed(error.message);
